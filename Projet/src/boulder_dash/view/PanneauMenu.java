5dd913eaed99e8a6508e0ee576ce1a0a43641159
tangi-mallegol <tangi.mallegol@gmail.com>
2015-05-29 17:06:09 +0200

    Push

---
+++ Projet/src/boulder_dash/view/PanneauMenu.java
@@ -0,0 +1,44 @@
+package boulder_dash.view;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+
+public class PanneauMenu {
+	
+	private JButton Editeur;
+	private JButton Jeu;
+	private EditeurListener EditeurListener;
+	private JeuListener JeuListener;
+	
+	public PanneauMenu(){
+		this.Editeur = new JButton();
+		this.Jeu = new JButton();
+		this.EditeurListener = new EditeurListener();
+		this.JeuListener = new JeuListener();
+	}
+	
+	private class EditeurListener implements ActionListener{
+		
+		public EditeurListener(){
+			super();
+		}
+		
+		public void actionPerformed(ActionEvent e) {
+			// On renvoi vers le panneau editeur
+		}
+	}
+	
+	
+	private class JeuListener implements ActionListener{
+		
+		public JeuListener(){
+			super();
+		}
+		
+		public void actionPerformed(ActionEvent e) {
+			// On renvoi vers le panneau de jeu
+		}
+	}
+}
