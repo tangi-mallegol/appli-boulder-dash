43e7f4888eee00cf1ff39b05cfeecb0ad43cf5df
Mathieu <Mathieu@PC-Mathieu>
2015-06-29 22:45:19 +0200

    Test résolution bug

--- Projet/src/boulderDash/vue/PanneauInfoJeu.java
+++ Projet/src/boulderDash/vue/PanneauInfoJeu.java
@@ -10,27 +10,39 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class PanneauInfoJeu extends JPanel implements Observer{
+	private JLabel temps, score;
+	
 	public PanneauInfoJeu(){
 		setBackground(Color.WHITE);
 		setSize(new Dimension(100,200));
 		setLayout(new GridLayout(2,1));
-		add(new JLabel("TEMPS : 00:00:00"));
-		add(new JLabel("SCORE : 0"));
+		
+		temps = new JLabel("TEMPS : 00:00:00");
+		score = new JLabel("SCORE : 0");
+		
+		add(temps);
+		add(score);
 		//add(new JLabel("BOULDER DASH POWER"));
 	}
 
 	public void update(Observable o, Object arg) {
 		System.out.println("update");
-		this.removeAll();
+		//this.removeAll();
 		Integer[] tableau_param = (Integer[])arg;
 		int secondes = tableau_param[1]%60;
 		int minutes = tableau_param[1]/60;
 		int heures = tableau_param[1]/3600;
 		//System.out.println("TEMPS : " + (heures < 10 ? "0" + heures : heures) +":" + (minutes < 10 ? "0" + minutes : minutes) + ":" + (secondes < 10 ? "0" + secondes : secondes));
-		System.out.println(this.countComponents());
-		add(new JLabel("TEMPS : " + (heures < 10 ? "0" + heures : heures) +":" + (minutes < 10 ? "0" + minutes : minutes) + ":" + (secondes < 10 ? "0" + secondes : secondes)));
-		add(new JLabel("SCORE : " + tableau_param[0]));
-		System.out.println(this.countComponents());
-		revalidate();
+		//System.out.println(this.countComponents());
+		temps.setText("TEMPS : " + (heures < 10 ? "0" + heures : heures) +":" + (minutes < 10 ? "0" + minutes : minutes) + ":" + (secondes < 10 ? "0" + secondes : secondes));
+		temps.repaint();
+		score.setText("SCORE : " + tableau_param[0]);
+		score.repaint();
+		
+		
+		
+		//repaint();
+		//System.out.println(this.countComponents());
+		//revalidate();
 	}
 }
